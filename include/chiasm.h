6a6ffbc77ccd202e2f2f5718ffda78f3081c90c2
zkingston <kingston.zak@gmail.com>
2015-07-15 14:20:34 -0500

    Initializing mmap regions.

--- include/chiasm.h
+++ include/chiasm.h
@@ -6,6 +6,11 @@
 #include <linux/types.h>
 #include <linux/videodev2.h>
 
+struct ch_buf {
+    void *start;
+    __u32 length;
+};
+
 __u32 string_to_pixfmt(char *buf);
 void pixfmt_to_string(__u32 pixfmt, char *buf);
 void print_v4l2_capability(struct v4l2_capability *caps);
--- src/stream.c
+++ src/stream.c
@@ -10,6 +10,7 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 
 #include <linux/types.h>
@@ -21,6 +22,7 @@
 #define CH_DEFAULT_FORMAT "YUYV"
 #define CH_DEFAULT_WIDTH  320
 #define CH_DEFAULT_HEIGHT 240
+#define CH_DEFAULT_BUFNUM 5
 
 bool verbose = false;
 bool list = false;
@@ -176,15 +178,77 @@ init_device(int fd, __u32 pixel_format, __u32 frame_width, __u32 frame_height)
     return (0);
 }
 
+int
+unmap_buffers(__u32 buffer_count, struct ch_buf *buffers)
+{
+    size_t idx;
+    for (idx = 0; idx < buffer_count; idx++) {
+	if (munmap(buffers[idx].start, buffers[idx].length) == -1) {
+	    fprintf(stderr, "Failed to munmap buffer. %d: %s\n",
+		    errno, strerror(errno));
+
+	    return (-1);
+	}
+    }
+
+    return (0);
+}
+
 /**
  * Initialize memory-mapped region for streaming video.
  */
 int
-init_mmap(int fd)
+init_mmap(int fd, __u32 buffer_count, struct ch_buf *buffers)
 {
     struct v4l2_requestbuffers req;
 
+    req.count = buffer_count;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
 
+    if (ioctl_r(fd, VIDIOC_REQBUFS, &req) == -1) {
+	fprintf(stderr, "Failed to request buffers.\n");
+	return (-1);
+    }
+
+    if (req.count != buffer_count) {
+	fprintf(stderr, "Insufficient buffer memory on device (%u vs. %u).\n",
+		buffer_count, req.count);
+	return (-1);
+    }
+
+    size_t idx;
+    for (idx = 0; idx < buffer_count; idx++) {
+	struct v4l2_buffer buf;
+
+	buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	buf.memory = V4L2_MEMORY_MMAP;
+	buf.index = idx;
+
+	if (ioctl_r(fd, VIDIOC_QUERYBUF, &buf) == -1) {
+	    fprintf(stderr, "Failed to query buffers.\n");
+	    return (-1);
+	}
+
+	buffers[idx].length = buf.length;
+	buffers[idx].start = mmap(
+	    NULL,
+	    buf.length,
+	    PROT_READ | PROT_WRITE,
+	    MAP_SHARED,
+	    fd,
+	    buf.m.offset
+	);
+
+	if (buffers[idx].start == MAP_FAILED) {
+	    fprintf(stderr, "Failed to mmap buffer. %d: %s\n",
+		    errno, strerror(errno));
+
+	    unmap_buffers(idx, buffers);
+
+	    return (-1);
+	}
+    }
 
     return (0);
 }
@@ -212,8 +276,8 @@ open_device(char *video_device)
 
     int fd;
 
-    // Open device in read/write nonblocking mode.
-    if ((fd = open(video_device, O_RDWR | O_NONBLOCK)) == -1) {
+    // Open device in read/write non-blocking mode.
+    if ((fd = open(video_device, O_RDWR | O_NONBLOCK )) == -1) {
 	fprintf(stderr, "Failed to open device. %d: %s\n",
 		errno, strerror(errno));
 	return (-1);
@@ -245,9 +309,10 @@ main(int argc, char *argv[])
     __u32 pixel_format = string_to_pixfmt(CH_DEFAULT_FORMAT);
     __u32 frame_width  = CH_DEFAULT_WIDTH;
     __u32 frame_height = CH_DEFAULT_HEIGHT;
+    __u32 buffer_count = CH_DEFAULT_BUFNUM;
 
     int opt;
-    char *opts = "d:f:g:s:lvh?";
+    char *opts = "b:d:f:g:s:lvh?";
     while ((opt = getopt(argc, argv, opts)) != -1) {
 	switch (opt) {
 	case 'd':
@@ -258,6 +323,15 @@ main(int argc, char *argv[])
 	    break;
 	case 'l':
 	    list = true;
+	    break;
+	case 'b':
+	    buffer_count = (__u32) strtoul(optarg, NULL, 10);
+	    if (errno == EINVAL || errno == ERANGE || buffer_count == 0) {
+		fprintf(stderr, "Invalid value in buffer count argument %s.\n",
+			optarg);
+		return (-1);
+	    }
+
 	    break;
 	case 'f':
 	    if (strnlen(optarg, 5) != 4) {
@@ -284,6 +358,7 @@ main(int argc, char *argv[])
 		"  -d    Device name. \"%s\" by default.\n"
 		"  -f    4-character image format code. %s by default.\n"
 		"  -g    Frame geometry in <w>x<h> format. %ux%u by default.\n"
+		"  -b    Specify number of buffers to request. %u by default.\n"
 		"  -l    List formats, resolutions, framerates and exit.\n"
 		"  -v    Enable verbose output.\n"
 		"  -?,h  Show this help.\n",
@@ -291,10 +366,18 @@ main(int argc, char *argv[])
 		CH_DEFAULT_DEVICE,
 		CH_DEFAULT_FORMAT,
 		CH_DEFAULT_WIDTH,
-		CH_DEFAULT_HEIGHT
+		CH_DEFAULT_HEIGHT,
+		CH_DEFAULT_BUFNUM
 	    );
-	    break;
+
+	    return (0);
 	}
+    }
+
+    struct ch_buf *buffers = calloc(buffer_count, sizeof(struct ch_buf));
+    if (buffers == NULL || errno == ENOMEM) {
+	fprintf(stderr, "No memory available.\n");
+	return (-1);
     }
 
     int r = 0;
@@ -314,12 +397,16 @@ main(int argc, char *argv[])
     if ((r = init_device(fd, pixel_format, frame_width, frame_height)) == -1)
 	goto cleanup;
 
-    if ((r = init_mmap(fd)) == -1)
+    if ((r = init_mmap(fd, buffer_count, buffers)) == -1)
 	goto cleanup;
 
 cleanup:
+    unmap_buffers(buffer_count, buffers);
+    free(buffers);
+
     if (fd > 0)
 	close_device(fd);
+
 
     return (r);
 }
