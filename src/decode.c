5a99f550d2c4960fe0b5eabecd068e482f98d789
zkingston <kingston.zak@gmail.com>
2015-07-23 15:41:17 -0500

    All decoding through one function.

--- src/decode.c
+++ src/decode.c
@@ -115,77 +115,41 @@ ch_destroy_decode_cx(struct ch_device *device)
 	sws_freeContext(cx->sws_cx);
 }
 
-static int
-ch_convert(struct ch_device *device)
-{
-    struct ch_decode_cx *cx = &device->decode_cx;
-
-    // Allocate the SWS context if we have not already.
-    if (cx->sws_cx == NULL) {
-	cx->sws_cx = sws_getContext(
-	    device->framesize.width,
-	    device->framesize.height,
-	    AV_PIX_FMT_YUYV422,
-	    device->framesize.width,
-	    device->framesize.height,
-	    cx->out_pixfmt,
-	    SWS_BILINEAR,
-	    NULL,
-	    NULL,
-	    NULL
-        );
-
-	if (cx->sws_cx == NULL) {
-	    ch_error("Failed to initialize SWS context.");
-	    return (-1);
-	}
-    }
-
-    if (avpicture_fill((AVPicture *) cx->frame_in,
-		       device->in_buffer->start, AV_PIX_FMT_YUYV422,
-		       device->framesize.width, device->framesize.height) < 0) {
-	ch_error("Failed to setup output frame fields.");
-	return (-1);
-    };
-
-    // Convert the image into a RGB24 array.
-    sws_scale(
-	cx->sws_cx,
-	(uint8_t const * const *) cx->frame_in->data,
-	cx->frame_in->linesize,
-	0,
-	device->framesize.height,
-	cx->frame_out->data,
-	cx->frame_out->linesize
-	);
-
-    return (0);
-}
-
 int
 ch_decode(struct ch_device *device)
 {
     struct ch_decode_cx *cx = &device->decode_cx;
 
-    // Use same SWS method, just convert.
-    if (!cx->compressed) {
-	ch_convert(device);
-	return (0);
-    }
-
-    // Initialize packet to use input buffer.
-    AVPacket packet;
-    av_init_packet(&packet);
-
-    packet.data = device->in_buffer->start;
-    packet.size = device->in_buffer->length;
-
+    enum AVPixelFormat in_pixfmt = AV_PIX_FMT_NONE;
     int finish = 0;
-    int r = 0;
-    if (avcodec_decode_video2(cx->codec_cx, cx->frame_in, &finish, &packet) < 0) {
-	ch_error("Failed decoding video.");
-	r = -1;
-	goto exit;
+
+    if (!cx->compressed) {
+	finish = 1;
+	in_pixfmt = AV_PIX_FMT_YUYV422;
+
+	if (avpicture_fill((AVPicture *) cx->frame_in,
+			   device->in_buffer->start, AV_PIX_FMT_YUYV422,
+			   device->framesize.width, device->framesize.height) < 0) {
+	    ch_error("Failed to setup output frame fields.");
+	    return (-1);
+
+	}
+    } else {
+	// Initialize packet to use input buffer.
+	AVPacket packet;
+	av_init_packet(&packet);
+
+	packet.data = device->in_buffer->start;
+	packet.size = device->in_buffer->length;
+
+	if (avcodec_decode_video2(cx->codec_cx, cx->frame_in, &finish, &packet) < 0) {
+	    ch_error("Failed decoding video.");
+	    av_free_packet(&packet);
+	    return (-1);
+	}
+
+	av_free_packet(&packet);
+	in_pixfmt = cx->codec_cx->pix_fmt;
     }
 
     // Convert image into RGB24 upon success.
@@ -193,11 +157,11 @@ ch_decode(struct ch_device *device)
 	// Allocate the SWS context if we have not already.
 	if (cx->sws_cx == NULL) {
 	    cx->sws_cx = sws_getContext(
-		cx->codec_cx->width,
-		cx->codec_cx->height,
-		cx->codec_cx->pix_fmt,
-		cx->codec_cx->width,
-		cx->codec_cx->height,
+		device->framesize.width,
+		device->framesize.height,
+		in_pixfmt,
+		device->framesize.width,
+		device->framesize.height,
 		cx->out_pixfmt,
 		SWS_BILINEAR,
 		NULL,
@@ -207,8 +171,7 @@ ch_decode(struct ch_device *device)
 
 	    if (cx->sws_cx == NULL) {
 		ch_error("Failed to initialize SWS context.");
-		r = -1;
-		goto exit;
+		return (-1);
 	    }
 	}
 
@@ -218,13 +181,11 @@ ch_decode(struct ch_device *device)
 	    (uint8_t const * const *) cx->frame_in->data,
 	    cx->frame_in->linesize,
 	    0,
-	    cx->codec_cx->height,
+	    device->framesize.height,
 	    cx->frame_out->data,
 	    cx->frame_out->linesize
 	);
     }
 
-exit:
-    av_free_packet(&packet);
-    return (r);
+    return (0);
 }
