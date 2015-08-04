0a298420c1317f2c36e35e61668013871180da12
zkingston <kingston.zak@gmail.com>
2015-08-04 14:51:40 -0500

    Publishing detections to ach channel.

--- src/plugin/apriltag.c
+++ src/plugin/apriltag.c
@@ -9,12 +9,20 @@
 #include <common/image_u8.h>
 #include <common/homography.h>
 
+#include <sns.h>
 #include <amino.h>
 
 #include <chiasm.h>
 
+#define MAX_TAG_ID 32
+#define TAG_SIZE_2 (41.0 / 2.0)
+
 apriltag_family_t *tag_family = NULL;
 apriltag_detector_t *tag_detector = NULL;
+
+const char *marker_chan_name = "markers";
+ach_channel_t marker_chan;
+ach_channel_t *sns_chans[] = { &marker_chan, NULL };
 
 uint32_t width, height, stride;
 
@@ -22,8 +30,8 @@ bool calib = false;
 double f[2];
 double c[2];
 
-void
-pose_to_qv(matd_t *p, double q[7])
+static void
+pose_to_q(matd_t *p, double q[4])
 {
     double R[9];
 
@@ -32,29 +40,29 @@ pose_to_qv(matd_t *p, double q[7])
         size_t jdx;
         for (jdx = 0; jdx < 3; jdx++)
             AA_MATREF(R, 3, idx, jdx) = matd_get(p, idx, jdx);
-
-        q[4 + idx] = matd_get(p, idx, 3);
     }
 
     aa_tf_rotmat2quat(R, q);
 }
 
-
 int
 CH_DL_INIT(struct ch_device *device, struct ch_dl_cx *cx)
 {
+    if (device->calib == NULL)
+        return (-1);
+
+    calib = true;
+    cx->undistort = true;
+
+    // Grab calibration parameters
+    size_t idx;
+    for (idx = 0; idx < 2; idx++) {
+        f[idx] = device->calib->camera_mat[idx][idx];
+        c[idx] = device->calib->camera_mat[idx][2];
+    }
+
     width = device->framesize.width;
     height = device->framesize.height;
-
-    if (device->calib) {
-        calib = true;
-        cx->undistort = true;
-        f[0] = device->calib->camera_mat[0][0];
-        f[1] = device->calib->camera_mat[1][1];
-
-        c[0] = device->calib->camera_mat[0][2];
-        c[1] = device->calib->camera_mat[1][2];
-    }
 
     cx->out_pixfmt = AV_PIX_FMT_GRAY8;
     stride = cx->out_stride = ch_calc_stride(cx, width, 96);
@@ -68,14 +76,25 @@ CH_DL_INIT(struct ch_device *device, struct ch_dl_cx *cx)
     tag_detector = apriltag_detector_create();
     apriltag_detector_add_family(tag_detector, tag_family);
 
-    tag_detector->quad_decimate = 3;
+    tag_detector->quad_decimate = width / 320.0;
     tag_detector->quad_sigma = 0.0;
-    tag_detector->nthreads = 2;
+    tag_detector->nthreads = 4;
     tag_detector->debug = 0;
 
     tag_detector->refine_edges = 1;
     tag_detector->refine_decode = 0;
     tag_detector->refine_pose = 0;
+
+    sns_init();
+
+    ach_attr_t attr;
+    ach_attr_init(&attr);
+    ach_attr_set_lock_source(&attr, 1);
+    sns_chan_open(&marker_chan, marker_chan_name, NULL);
+
+    sns_sigcancel(sns_chans, sns_sig_term_default);
+
+    sns_start();
 
     return (0);
 }
@@ -83,6 +102,9 @@ CH_DL_INIT(struct ch_device *device, struct ch_dl_cx *cx)
 int
 CH_DL_CALL(struct ch_frmbuf *in_buf)
 {
+    if (sns_cx.shutdown)
+        return (-1);
+
     image_u8_t image = {
         .width = width,
         .height = height,
@@ -92,29 +114,42 @@ CH_DL_CALL(struct ch_frmbuf *in_buf)
 
     zarray_t *detections = apriltag_detector_detect(tag_detector, &image);
 
-    if (!zarray_size(detections))
-        return (0);
+    struct timespec now = sns_now();
+    struct sns_msg_wt_tf *msg = sns_msg_wt_tf_local_alloc(MAX_TAG_ID);
+    sns_msg_set_time(&msg->header, &now, 0);
 
     int i;
     for (i = 0; i < zarray_size(detections); i++) {
         apriltag_detection_t *det;
         zarray_get(detections, i, &det);
 
-        if (calib) {
-            matd_t *pose = homography_to_pose(det->H,
-                                              f[0], f[1], c[0], c[1]);
-
-            double q[7];
-            pose_to_qv(pose, q);
-
-            size_t idx;
-            for (idx = 0; idx < 7; idx++)
-                fprintf(stderr, "%f ", q[idx]);
-            fprintf(stderr, "\n");
+        if (det->id > MAX_TAG_ID) {
+            fprintf(stderr, "Tag ID greater than message size.\n");
+            return (-1);
         }
+
+        matd_t *pose = homography_to_pose(det->H,
+                                          f[0], f[1], c[0], c[1]);
+
+        double q[4];
+        pose_to_q(pose, q);
+
+        sns_wt_tf *wt_tf = &(msg->wt_tf[det->id]);
+
+        size_t idx;
+        for (idx = 0; idx < 3; idx++)
+            wt_tf->tf.v.data[idx] = matd_get(pose, idx, 3) * TAG_SIZE_2;
+
+        for (idx = 0; idx < 4; idx++)
+            wt_tf->tf.r.data[idx] = q[idx];
+
+        wt_tf->weight = det->decision_margin;
     }
 
-    fprintf(stderr, "Tag!\n");
+    enum ach_status r = sns_msg_wt_tf_put(&marker_chan, msg);
+    if (ACH_OK != r)
+        ch_error("Failed to put marker message in ach channel.");
+
 
     apriltag_detections_destroy(detections);
 
